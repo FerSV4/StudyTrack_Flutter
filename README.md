0d1965ec35be25bc63c08e2f93db077d1293b2e8
FerSV4 <ferandressc@hotmail.com>
2026-06-09 14:32:30 -0400

    chore: inicializacion del proyecto y estructura Clean Architecture

---
+++ README.md
@@ -0,0 +1,3 @@
+# studytrack_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/studytrack_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.studytrack_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
